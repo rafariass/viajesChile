11de86bc7f63dab5248ec15bc102c3eb68c4c20b
Raul Farias S <ra.farias.s@outlook.com>
2021-05-13 11:52:52 -0400

    footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,15 @@
 $(function () {
     // Define en segundos el intervalo del carousel
     $('.carousel').carousel({ interval: 2000 });
+
+    // Habilita los tooltip dentro del proyecto
+    $('[data-toggle="tooltip"]').tooltip()
 });
 
 // Controla el scroll de la pag. para agregar o quitar las clases del navbar
-window.addEventListener("scroll", function () {
-    const header = document.querySelector("header");
-    header.classList.toggle("bg-dark", window.scrollY > 1);
-    header.classList.toggle("header-border", window.scrollY > 1);
-    header.classList.toggle("header-pt", window.scrollY < 1);
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    header.classList.toggle('bg-dark', window.scrollY > 1);
+    header.classList.toggle('header-border', window.scrollY > 1);
+    header.classList.toggle('header-pt', window.scrollY < 1);
 });
